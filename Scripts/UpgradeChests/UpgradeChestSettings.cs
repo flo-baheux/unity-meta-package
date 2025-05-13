b153d3b98452309f10321e44ef27aff16b0b3776
Laywelin - Florian Baheux <florian.baheux@gmail.com>
2025-05-13 21:08:27 +0200

    Add validation to upgradechestsettings

--- Scripts/UpgradeChests/UpgradeChestSettings.cs
+++ Scripts/UpgradeChests/UpgradeChestSettings.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace MetaPackage
 {
   [CreateAssetMenu(menuName = "MetaPackage/UpgradeChest", fileName = "UpgradeChestSettings")]
-  public class UpgradeChestSettings : ScriptableObject
+  public class UpgradeChestSettings : ValidatedScriptableObject
   {
     public bool isCustom = false;
 
@@ -23,5 +23,28 @@ namespace MetaPackage
     // FIXME: Custom UpgradeChestSettings editor to hide the wrong one
     public List<RarityUpgradeChestData> rarityUpgradeChestDatas;
     public List<CustomUpgradeChestData> customUpgradeChestDatas;
+
+    public override void CustomValidation()
+    {
+      if (HasNonCompatibleCustomUpgradableKind(out string nonCompatibleUpgradableKindErrorMessage))
+        errors.Add(nonCompatibleUpgradableKindErrorMessage);
+    }
+
+    private bool HasNonCompatibleCustomUpgradableKind(out string errorMessage)
+    {
+      errorMessage = $"Custom upgrades are not compatible (not of upgradable kind {upgradableKind}).";
+      bool foundNotCompatible = false;
+      if (!isCustom)
+        return false;
+
+      foreach (var customUpgradeChestData in customUpgradeChestDatas)
+        if (customUpgradeChestData.upgradable.UpgradableKind != upgradableKind)
+        {
+          foundNotCompatible = true;
+          errorMessage += $"\n{customUpgradeChestData.upgradable.name} ({customUpgradeChestData.upgradable.UpgradableKind})";
+        }
+
+      return foundNotCompatible;
+    }
   }
 }
